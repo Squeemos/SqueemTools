465de8d403c248acf61282948fc0937152852396
Ben <squeemos@gmai.com>
2020-11-06 19:54:32 -0800

    Trying to get this working now

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
   author = 'Ben van Oostendorp',                   # Type in your name
   author_email = 'ben.vano@digipen.edu',      # Type in your E-Mail
   url = 'https://github.com/Squeemos/SqueemTools',   # Provide either the link to your github or to your website
-  download_url = 'https://github.com/Squeemos/SqueemTools/archive/SqueemTools_1.0.tar.gz',    # I explain this later on
+  download_url = 'https://github.com/Squeemos/SqueemTools/archive/SqueemTools_1.0.1.tar.gz',    # I explain this later on
   keywords = ['GRAPHING', 'FUZZY C MEANS'],   # Keywords that define your package best
   install_requires=[            # I get to this in a second
           'matplotlib',
