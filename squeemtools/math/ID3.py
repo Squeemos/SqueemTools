bf00aa205e5a8d02d3242df7bb17644a3f59ecc8
Ben <squeemos@gmai.com>
2021-09-29 00:16:40 -0700

    Update of a few things
    Added in annotations for ID3, made the kmeans 1-liner better (and worse at the same time), also added in getting max value from dictionaries.

--- squeemtools/math/ID3.py
+++ squeemtools/math/ID3.py
@@ -2,30 +2,30 @@ import pandas as pd
 import numpy as np
 from scipy.stats import entropy
 
-def total_entropy(df):
-    '''Gets the entropy of the entire data'''
+def total_entropy(df : pd.DataFrame) -> float:
+    '''Gets the entropy of the entire dataset'''
     label = df.keys()[-1]
 
     # Gets the total entropy of the dataframe
     ent = entropy(df[label].value_counts(),base=2)
     return ent
 
-def attribute_entropy(df,attribute):
+def attribute_entropy(df : pd.DataFrame,attribute : str) -> float:
     '''Gets the entropy of a specific attribute'''
     label = df.keys()[-1]
     variables = df[attribute].unique()
 
     # Gets the entropy of the attribute
-    ent = np.array([-(len(df[df[attribute] == variable]) / len(df)) * entropy(df[label][df[attribute]==variable].value_counts(),base=2) for variable in variables]).sum()
+    ent = np.array([-(len(df[df[attribute] == variable]) / len(df)) * entropy(df[df[attribute]==variable][label].value_counts(),base=2) for variable in variables]).sum()
     return abs(ent)
 
-def create_tree(df):
+def create_tree(df : pd.DataFrame) -> dict:
     '''ID3 decision tree creating algorithm. Requires df to be a pd.DataFrame, and that the label of the data is the last column'''
     label = df.keys()[-1]
     #Get attribute with maximum information gain
     node = df.keys()[:-1][np.argmax([total_entropy(df)-attribute_entropy(df,key) for key in df.keys()[:-1]])]
 
-    #Get distinct value of that attribute e.g Salary is node and Low,Med and High are values
+    #Get distinct value of that attribute
     att_values = df[node].unique()
     # Create the tree
     tree={}
@@ -45,14 +45,15 @@ def create_tree(df):
             tree[node][att_value] = create_tree(subtable)
     return tree
 
-def is_number(s):
+def is_number(s) -> bool:
     try:
         float(s)
         return True
     except ValueError:
         return False
 
-def go(dic, last_key, current_level):
+def go(dic : dict, last_key : None, current_level : None) -> None:
+    '''Prints the tree, best used with the wrapper. ONLY WORKS ON TREES WITH NUMERIC ENTRIES'''
     for key, value in dic.items():
         if is_number(key):
             for i in range(current_level - 1):
@@ -71,6 +72,17 @@ def go(dic, last_key, current_level):
         else:
             print(value)
 
-def print_tree(tree):
-    '''Prints the tree created with ID3 algorithm'''
+def print_tree(tree : dict) -> None:
+    '''Prints the tree created with ID3 algorithm
+       ONLY PRINTS PROPERLY WITH TREES WITH NUMERIC NODE ENTRIES'''
     go(tree,None,0)
+
+def classify_series(data : pd.Series,tree : dict):
+    '''Given a series, returns the tree's classification of the series
+       data is a pd.Series object, and tree is a dict created with the ID3 algorithm'''
+    label = tree
+    while isinstance(label,dict):
+        key = list(label.keys())[0]
+        value = data[key]
+        label = label[key][value]
+    return label
--- squeemtools/other/Dictionaries.py
+++ squeemtools/other/Dictionaries.py
@@ -1,4 +1,13 @@
+import operator
+
 def sort_dict_by_values(dictionary,asc=False):
     '''Takes a dictionary and returns it sorted in descending or ascending order'''
     if asc: return {k: v for k, v in sorted(dictionary.items(), key=lambda item: item[1])}
     else: return {k: v for k, v in sorted(dictionary.items(), key=lambda item: item[1],reverse=True)}
+
+def get_dict_max(dictionary,**kwargs):
+    assert 'how' in kwargs, "Need to pass 'how' = 'key' or 'value' so a max can be obtained."
+    if kwargs['how'] == 'key':
+        return max(test.items(), key=operator.itemgetter(0))[1]
+    elif kwargs['how'] == 'value':
+        return max(test.items(), key=operator.itemgetter(1))[0]
